a4d5b707879fb03922da7982241791c91894cac3
javierspr <learn.javier@gmail.com>
2024-05-11 20:33:53 -0300

    update ul & li

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,19 @@ function App() {
       <ThemeBtn/>
       <div className="flex justify-between items-center md:min-w-[30rem] bg-neutral-200 rounded-md p-2">
         <input className="min-w-[90%] outline-none border-none bg-transparent text-xl py-1 text-black" type="text" placeholder="Password" id="userOutput"/>
-        <i className="fa-solid fa-copy text-2xl text-blue-600 cursor-pointer"></i>
+        <i className="fa-solid fa-copy text-2xl text-blue-600 hover:text-blue-500 cursor-pointer"></i>
       </div>
       <div className="flex flex-col font-semibold gap-2">
         <label htmlFor="customRange">Password Length: <span>0</span></label> 
         <input className="cursor-pointer" type="range" id="customRange" min="0" max="30"/>
       </div>
-      <ul>
+      <ul className="space-y-8">
         <li>Uppercase (A-Z)</li>
         <li>Lowercase (a-z)</li>
         <li>Symbol (!$#@)</li>
         <li>Number (0-9)</li>
       </ul>
-      <button className="flex mx-auto gap-1 w-full text-xl py-2 rounded-md items-center bg-blue-500 text-white justify-center" id="generatePw">Generate Password <i className="fa-solid fa-rotate-left"></i></button>
+      <button className="flex mx-auto gap-1 w-full text-xl py-3 rounded-md items-center bg-blue-500 hover:bg-blue-400 text-white justify-center" id="generatePw">Generate Password <i className="fa-solid fa-rotate-left"></i></button>
     </div>
   )
 }
--- src/components/LinkGithub.tsx
+++ src/components/LinkGithub.tsx
@@ -1,7 +1,7 @@
 export default function LinkGithub() {
     return (
       <a href="https://github.com/codebyjavi/pw-gen-ts" target="_blank" 
-      className="text-3xl absolute -top-20 -left-5 bg-blue-500 pl-10 py-20 pr-1 rotate-45"
+      className="text-3xl absolute -top-20 -left-5 bg-blue-500 hover:bg-blue-400 pl-10 py-20 pr-1 rotate-45"
       >
           <i className="fa-brands fa-github -rotate-45"></i>
       </a>
