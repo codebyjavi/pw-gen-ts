5669d1702759ae087c2a60163cca34a37f02bca3
javierspr <learn.javier@gmail.com>
2024-05-11 20:14:03 -0300

    update LinkGithub

--- src/components/LinkGithub.tsx
+++ src/components/LinkGithub.tsx
@@ -1,6 +1,6 @@
 export default function LinkGithub() {
     return (
-      <a href="https://github.com/javxdev/pw-gen-ts" target="_blank" 
+      <a href="https://github.com/codebyjavi/pw-gen-ts" target="_blank" 
       className="text-3xl absolute -top-20 -left-5 bg-blue-500 pl-10 py-20 pr-1 rotate-45"
       >
           <i className="fa-brands fa-github -rotate-45"></i>
