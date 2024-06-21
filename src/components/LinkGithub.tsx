b2d9d84f6b3782186ce82197ad6b874c259d5a48
javierspr <learn.javier@gmail.com>
2024-06-21 00:54:29 -0300

    delete LinkGithub

--- src/components/LinkGithub.tsx
+++
@@ -1,9 +0,0 @@
-export default function LinkGithub() {
-    return (
-      <a href="https://github.com/codebyjavi/pw-gen-ts" target="_blank" 
-        className="text-3xl absolute -top-20 -left-5 bg-blue-500 hover:bg-blue-400 pl-10 py-20 pr-1 rotate-45"
-      >
-          <i className="fa-brands fa-github -rotate-45"></i>
-      </a>
-    )
-}
